bda094794d0c213ce3c11c6e76b1e96e9a519f7f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-03-05 23:02:17 -0500

    started assembling machine recipes

--- src/main/java/trinsdar/gt4r/loader/machines/AssemblyLoader.java
+++ src/main/java/trinsdar/gt4r/loader/machines/AssemblyLoader.java
@@ -14,6 +14,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.tags.ItemTags;
+import trinsdar.gt4r.data.GregTechData;
 
 import java.util.Arrays;
 import java.util.Set;
@@ -21,6 +22,7 @@ import java.util.Set;
 import static com.google.common.collect.ImmutableMap.of;
 import static muramasa.antimatter.Data.*;
 import static muramasa.antimatter.Data.WRENCH;
+import static muramasa.antimatter.recipe.ingredient.AntimatterIngredient.of;
 import static trinsdar.gt4r.data.GregTechData.*;
 import static trinsdar.gt4r.data.Materials.*;
 import static trinsdar.gt4r.data.RecipeMaps.ASSEMBLING;
@@ -34,8 +36,12 @@ public class AssemblyLoader {
             sizes.forEach(size -> {
                 Item wireItem = t.getBlockItem(size);
                 Item cableItem = cable.getBlockItem(size);
-                ASSEMBLING.RB().ii(AntimatterIngredient.of(wireItem,1), INT_CIRCUITS.get(24)).fi(Rubber.getLiquid(size.getCableThickness()*16)).io(new ItemStack(cableItem,1)).add(size.getCableThickness()* 20L,8);
+                ASSEMBLING.RB().ii(of(wireItem,1), of(INGOT.get(Rubber, size.getCableThickness()))).io(new ItemStack(cableItem,1)).add(size.getCableThickness()* 20L,8);
             });
         });
+        ASSEMBLING.RB().ii(of(Items.STICK, 1), of(Items.COAL, 1)).io(new ItemStack(Items.TORCH, 4)).add(400, 1);
+        ASSEMBLING.RB().ii(of(Items.STRING, 1), of(Items.SLIME_BALL, 1)).io(new ItemStack(Items.LEAD, 2)).add(200, 2);
+        ASSEMBLING.RB().ii(of(CircuitBoardAdv, 1), of(AdvCircuitParts, 2)).io(new ItemStack(CircuitAdv, 1)).add(1600, 2);
+        ASSEMBLING.RB().ii(of(CircuitBoardProcessor, 1), of(CircuitDataStorage, 1)).io(new ItemStack(CircuitDataControl, 2)).add(3200, 4);
     }
 }
