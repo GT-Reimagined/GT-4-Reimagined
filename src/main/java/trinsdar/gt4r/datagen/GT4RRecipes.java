5bb8fdf63fb707d3129014215ecd61c45f38e76f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-03-12 13:08:30 -0500

    changed all recipes to use the new methods added

--- src/main/java/trinsdar/gt4r/datagen/GT4RRecipes.java
+++ src/main/java/trinsdar/gt4r/datagen/GT4RRecipes.java
@@ -3,6 +3,7 @@ package trinsdar.gt4r.datagen;
 import muramasa.antimatter.datagen.providers.AntimatterRecipeProvider;
 import trinsdar.gt4r.Ref;
 import trinsdar.gt4r.data.GT4RData;
+import trinsdar.gt4r.loader.crafting.CraftingHelper;
 import trinsdar.gt4r.loader.crafting.Machines;
 import trinsdar.gt4r.loader.crafting.Parts;
 import trinsdar.gt4r.loader.crafting.WireCablesPlates;
@@ -17,6 +18,7 @@ import java.util.function.Consumer;
 import static com.google.common.collect.ImmutableMap.of;
 import static muramasa.antimatter.Data.*;
 import static muramasa.antimatter.util.Utils.getForgeItemTag;
+import static trinsdar.gt4r.loader.crafting.CraftingHelper.criterion;
 
 public class GT4RRecipes extends AntimatterRecipeProvider {
 
@@ -36,14 +38,14 @@ public class GT4RRecipes extends AntimatterRecipeProvider {
     @Override
     public void registerRecipes(Consumer<IFinishedRecipe> consumer) {
         super.registerRecipes(consumer);
-        addConditionalRecipe(consumer, getStackRecipe("", "has_sulfur_dust", this.hasItem(getForgeItemTag("dusts/sulfur")),
+        addConditionalRecipe(consumer, getStackRecipe("", "has_sulfur_dust", criterion(getForgeItemTag("dusts/sulfur"), this),
                 new ItemStack(Blocks.TORCH, 6), of('D', getForgeItemTag("dusts/sulfur"), 'R', Tags.Items.RODS_WOODEN), "D", "R"), Ref.class, "sulfurTorch", Ref.ID, "sulfur_torch");
 
-        addItemRecipe(consumer, Ref.ID, "hopper", "", "has_wrench", this.hasItem(WRENCH.getTag()),
+        addItemRecipe(consumer, Ref.ID, "hopper", "", "has_wrench", criterion(WRENCH.getTag(), this),
                 Blocks.HOPPER, of('C', Blocks.CHEST, 'I', getForgeItemTag("plates/iron"), 'W', WRENCH.getTag()), "IWI", "ICI", " I ");
 
 
-        addItemRecipe(consumer, "gears", "has_stone", this.hasItem(Blocks.PISTON), Blocks.STICKY_PISTON, of('S', GT4RData.StickyResin, 'P', Blocks.PISTON), "S", "P");
+        addItemRecipe(consumer, "gears", "has_stone", criterion(Blocks.PISTON, this), Blocks.STICKY_PISTON, of('S', GT4RData.StickyResin, 'P', Blocks.PISTON), "S", "P");
     }
 
     @Override
