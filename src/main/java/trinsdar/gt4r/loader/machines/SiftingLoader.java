97fb90ce1db537a815958b4905a363327b7acd34
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-03-14 17:42:43 -0400

    changed a couple inputs to tags

--- src/main/java/trinsdar/gt4r/loader/machines/SiftingLoader.java
+++ src/main/java/trinsdar/gt4r/loader/machines/SiftingLoader.java
@@ -2,6 +2,7 @@ package trinsdar.gt4r.loader.machines;
 
 import muramasa.antimatter.Data;
 import muramasa.antimatter.recipe.ingredient.AntimatterIngredient;
+import muramasa.antimatter.util.Utils;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
@@ -23,6 +24,6 @@ public class SiftingLoader {
         // for when ic2 comes along to 1.16
         //SIFTING.RB().ii(of(CRUSHED_PURIFIED.getMaterialTag(Iridium), 1)).io(new ItemStack(Items.Iridium_ore, 1), new ItemStack(Items.Iridium_ore, 1), new ItemStack(Items.Iridium_ore, 1), new ItemStack(Items.Iridium_ore, 1), new ItemStack(Items.Iridium_ore, 1), DUST.get(Iridium, 1)).chances(1, 4, 15, 20, 40, 50).add(800, 16);
         SIFTING.RB().ii(of(CRUSHED_PURIFIED.getMaterialTag(Lapis), 1)).io(new ItemStack(Items.LAPIS_LAZULI, 1), new ItemStack(Items.LAPIS_LAZULI, 1), new ItemStack(Items.LAPIS_LAZULI, 1), new ItemStack(Items.LAPIS_LAZULI, 1), new ItemStack(Items.LAPIS_LAZULI, 1), DUST.get(Lapis, 1)).chances(1, 4, 15, 20, 40, 50).add(800, 16);
-        SIFTING.RB().ii(of(Items.GRAVEL, 1)).io(new ItemStack(Items.FLINT, 1)).add(800, 16);
+        SIFTING.RB().ii(of(Utils.getForgeItemTag("gravel"), 1)).io(new ItemStack(Items.FLINT, 1)).add(800, 16);
     }
 }
