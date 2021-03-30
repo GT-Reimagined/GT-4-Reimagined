f43c22368a04ab553484d80806552cb8551eae46
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-03-29 23:05:17 -0400

    fixed drum not dropping

--- src/main/java/trinsdar/gt4r/datagen/GT4RBlockLootProvider.java
+++ src/main/java/trinsdar/gt4r/datagen/GT4RBlockLootProvider.java
@@ -1,7 +1,10 @@
 package trinsdar.gt4r.datagen;
 
+import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.datagen.providers.AntimatterBlockLootProvider;
+import muramasa.antimatter.machine.BlockMachine;
 import net.minecraft.data.DataGenerator;
+import trinsdar.gt4r.block.BlockMachineMaterial;
 import trinsdar.gt4r.data.GT4RData;
 
 public class GT4RBlockLootProvider extends AntimatterBlockLootProvider {
@@ -12,6 +15,7 @@ public class GT4RBlockLootProvider extends AntimatterBlockLootProvider {
     @Override
     protected void loot() {
         super.loot();
+        AntimatterAPI.all(BlockMachineMaterial.class, providerDomain, this::add);
         tables.put(GT4RData.RUBBER_LEAVES, b -> droppingWithChancesAndSticks(GT4RData.RUBBER_LEAVES, GT4RData.RUBBER_SAPLING, 0.025F, 0.027777778F, 0.03125F, 0.041666668F, 0.1F));
         this.add(GT4RData.RUBBER_LOG);
         this.add(GT4RData.RUBBER_SAPLING);
