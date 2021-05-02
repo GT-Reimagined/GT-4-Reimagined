3c2b9fc7ea8b2326c4af98cd1bbdf1ba8f0b6464
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-05-02 19:31:41 -0400

    fixed quantum tank internal tank not counting as output tank too

--- src/main/java/trinsdar/gt4r/tile/single/TileEntityQuantumTank.java
+++ src/main/java/trinsdar/gt4r/tile/single/TileEntityQuantumTank.java
@@ -1,14 +1,34 @@
 package trinsdar.gt4r.tile.single;
 
+import muramasa.antimatter.capability.fluid.FluidTanks;
 import muramasa.antimatter.capability.machine.MachineFluidHandler;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.tile.TileEntityMachine;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+
+import javax.annotation.Nullable;
 
 public class TileEntityQuantumTank extends TileEntityMachine {
 
     public TileEntityQuantumTank(Machine<?> type) {
         super(type);
-        this.fluidHandler = LazyOptional.of(() -> new MachineFluidHandler<>(this, Integer.MAX_VALUE, 265000));
+        this.fluidHandler = LazyOptional.of(() -> new MachineFluidHandler<TileEntityQuantumTank>(this, Integer.MAX_VALUE, 265000){
+            @Nullable
+            @Override
+            public FluidTanks getOutputTanks() {
+                return super.getInputTanks();
+            }
+
+            @Override
+            protected FluidTank getTank(int tank) {
+                return getInputTanks().getTank(tank);
+            }
+
+            @Override
+            public FluidTanks getTanks(int tank) {
+                return getInputTanks();
+            }
+        });
     }
 }
