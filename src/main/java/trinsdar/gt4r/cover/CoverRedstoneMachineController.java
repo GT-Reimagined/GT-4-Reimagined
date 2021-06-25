685946cafbe192e1a79c56c6ac23af657dc99be3
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-06-24 20:24:04 -0400

    updated antimatter, more work on redstone controller cover

--- src/main/java/trinsdar/gt4r/cover/CoverRedstoneMachineController.java
+++ src/main/java/trinsdar/gt4r/cover/CoverRedstoneMachineController.java
@@ -1,4 +1,28 @@
 package trinsdar.gt4r.cover;
 
-public class CoverRedstoneMachineController {
+import muramasa.antimatter.cover.BaseCover;
+import muramasa.antimatter.cover.CoverStack;
+import net.minecraft.util.Direction;
+import trinsdar.gt4r.Ref;
+
+public class CoverRedstoneMachineController extends BaseCover {
+    public CoverRedstoneMachineController(){
+        super();
+        register();
+    }
+
+    @Override
+    public String getDomain() {
+        return Ref.ID;
+    }
+
+    @Override
+    public String getId() {
+        return "redstone_machine_controller";
+    }
+
+    @Override
+    public void onBlockUpdate(CoverStack<?> instance, Direction side) {
+
+    }
 }
