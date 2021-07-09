9527d4ee8c9ba8c8628bd1f554167aace3fb43b2
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-07-08 21:54:38 -0400

    fixed generators outputting to wrong face

---
+++ src/main/java/trinsdar/gt4r/tile/single/TileEntityCoveredGenerator.java
@@ -0,0 +1,30 @@
+package trinsdar.gt4r.tile.single;
+
+import muramasa.antimatter.capability.machine.MachineEnergyHandler;
+import muramasa.antimatter.machine.types.Machine;
+import muramasa.antimatter.tile.single.TileEntityGenerator;
+import net.minecraft.util.Direction;
+
+import static muramasa.antimatter.machine.MachineFlag.GENERATOR;
+
+public class TileEntityCoveredGenerator extends TileEntityGenerator<TileEntityCoveredGenerator> {
+    public TileEntityCoveredGenerator(Machine<?> type) {
+        super(type);
+        energyHandler.set(() -> new MachineEnergyHandler<TileEntityCoveredGenerator>(this, type.amps(),type.has(GENERATOR)){
+            @Override
+            public boolean canInput(Direction direction) {
+                return false;
+            }
+            @Override
+            public boolean canInput() {
+                return false;
+            }
+
+            @Override
+            public boolean canOutput(Direction direction) {
+                return super.canOutput(direction) && direction == tile.getOutputFacing();
+
+            }
+        });
+    }
+}
