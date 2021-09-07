9c1b8950057b69b1db6a632f413c3eb6b47e2b68
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-09-07 12:43:09 -0400

    added player messages and made it able to emit energy

--- src/main/java/trinsdar/gt4r/tile/single/TileEntityItemFilter.java
+++ src/main/java/trinsdar/gt4r/tile/single/TileEntityItemFilter.java
@@ -1,6 +1,7 @@
 package trinsdar.gt4r.tile.single;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import muramasa.antimatter.capability.machine.MachineEnergyHandler;
 import muramasa.antimatter.gui.SlotType;
 import muramasa.antimatter.gui.event.GuiEvent;
 import muramasa.antimatter.gui.event.IGuiEvent;
@@ -11,6 +12,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import tesseract.api.capability.TesseractGTCapability;
@@ -19,14 +21,25 @@ import trinsdar.gt4r.data.SlotTypes;
 import java.util.List;
 import java.util.Objects;
 
+import static muramasa.antimatter.machine.MachineFlag.ENERGY;
+import static muramasa.antimatter.machine.MachineFlag.GENERATOR;
+
 public class TileEntityItemFilter extends TileEntityMachine<TileEntityItemFilter> {
-    boolean blacklist;
-    boolean nbt;
-    boolean outputRedstone;
-    boolean invertRedstone;
-    boolean emitEnergy;
+    boolean blacklist = false;
+    boolean nbt = true;
+    boolean outputRedstone = false;
+    boolean invertRedstone = false;
+    boolean emitEnergy = false;
     public TileEntityItemFilter(Machine<?> type) {
         super(type);
+        if (type.has(ENERGY)) {
+            energyHandler.set(() -> new MachineEnergyHandler<TileEntityItemFilter>(this, 0L, this.getMachineTier().getVoltage() * 66L, this.getMachineTier().getVoltage(), this.getMachineTier().getVoltage(), 1, 1){
+                @Override
+                public boolean canOutput(Direction direction) {
+                    return super.canOutput(direction) && direction == tile.getFacing().getOpposite() && tile.emitEnergy;
+                }
+            });
+        }
     }
 
     public boolean accepts(ItemStack stack){
@@ -37,8 +50,7 @@ public class TileEntityItemFilter extends TileEntityMachine<TileEntityItemFilter
                 ItemStack slot = outputs.getStackInSlot(i);
                 if (!slot.isEmpty()) {
                     if (slot.getItem() == stack.getItem()){
-                        boolean add = !nbt || Objects.equals(slot.getTag(), stack.getTag());
-                        if (add) {
+                        if (!nbt || Objects.equals(slot.getTag(), stack.getTag())) {
                             list.add(slot.copy().getItem());
                         }
                     }
@@ -55,19 +67,23 @@ public class TileEntityItemFilter extends TileEntityMachine<TileEntityItemFilter
             switch (data[0]) {
                 case 0:
                     emitEnergy = !emitEnergy;
-                    playerEntity.sendMessage(new StringTextComponent( (emitEnergy ? "Don't emit energy" : "Emit energy to output side")), playerEntity.getUniqueID());
+                    playerEntity.sendMessage(new StringTextComponent( (emitEnergy ? "Emit energy to output side" : "Don't emit energy")), playerEntity.getUniqueID());
                     break;
                 case 1:
                     outputRedstone = !outputRedstone;
+                    playerEntity.sendMessage(new StringTextComponent( (outputRedstone ? "Emit redstone if slots contain something" : "Don't emit redstone")), playerEntity.getUniqueID());
                     break;
                 case 2:
                     invertRedstone = !invertRedstone;
+                    playerEntity.sendMessage(new StringTextComponent( (invertRedstone ? "I" : "Don't i") + "nvert redstone"), playerEntity.getUniqueID());
                     break;
                 case 3:
                     blacklist = !blacklist;
+                    playerEntity.sendMessage(new StringTextComponent( (blacklist ? "I" : "Don't i") + "nvert filter"), playerEntity.getUniqueID());
                     break;
                 case 4:
                     nbt = !nbt;
+                    playerEntity.sendMessage(new StringTextComponent( (nbt ? "NBT has to match" : "Ignore NBT")), playerEntity.getUniqueID());
                     break;
             }
         }
