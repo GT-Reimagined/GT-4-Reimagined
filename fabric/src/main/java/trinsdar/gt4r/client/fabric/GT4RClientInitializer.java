063cdd1c101af5ad798eafc8dfa675704fe3e502
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-09-25 16:56:51 -0400

    switched to antimatter client initializer

--- fabric/src/main/java/trinsdar/gt4r/client/fabric/GT4RClientInitializer.java
+++ fabric/src/main/java/trinsdar/gt4r/client/fabric/GT4RClientInitializer.java
@@ -1,11 +1,12 @@
 package trinsdar.gt4r.client.fabric;
 
 import io.github.fabricators_of_create.porting_lib.event.client.TextureStitchCallback;
+import muramasa.antimatter.client.fabric.IAntimatterClientInitializer;
 import net.fabricmc.api.ClientModInitializer;
 import trinsdar.gt4r.events.ClientEvents;
 import trinsdar.gt4r.proxy.ClientHandler;
 
-public class GT4RClientInitializer implements ClientModInitializer {
+public class GT4RClientInitializer implements IAntimatterClientInitializer {
     @Override
     public void onInitializeClient() {
         ClientHandler.setup();
