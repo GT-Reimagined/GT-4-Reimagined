c727a690fa6f50e024a1e106bac64466cb775979
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-11-20 21:12:39 -0500

    fixed remaining ids and alloy smelter init method

--- common/src/main/java/trinsdar/gt4r/loader/machines/AlloySmelterLoader.java
+++ common/src/main/java/trinsdar/gt4r/loader/machines/AlloySmelterLoader.java
@@ -16,19 +16,19 @@ import static trinsdar.gt4r.data.Materials.*;
 import static trinsdar.gt4r.data.RecipeMaps.ALLOY_SMELTING;
 
 public class AlloySmelterLoader {
-    public static void init(Consumer<FinishedRecipe> consumer, AntimatterRecipeProvider provider){
+    public static void init(){
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), INGOT.getMaterialIngredient(Tin, 1)).io(INGOT.get(Bronze, 3)).add("bronze_ingot_from_tetra", 150, 16);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), INGOT.getMaterialIngredient(Zinc, 1)).io(INGOT.get(Brass, 3)).add("brass_ingot_from_tetra",150, 16);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), DUST.getMaterialIngredient(Tin, 1)).io(INGOT.get(Bronze, 3)).add("bronze_ingot_from_tetra_1",150, 16);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Tetrahedrite, 3), DUST.getMaterialIngredient(Zinc, 1)).io(INGOT.get(Brass, 3)).add("brass_ingot_from_tetra_1",150, 16);
-        addAlloyRecipes(consumer, Copper, 3, Tin, 1, Bronze, 4, 200);
-        addAlloyRecipes(consumer, Copper, 3, Zinc, 1, Brass, 4, 200);
-        addAlloyRecipes(consumer, Copper, 1, Nickel, 1, Cupronickel, 2, 100);
-        addAlloyRecipes(consumer, Iron, 2, Nickel, 1, Invar, 3, 150);
-        addAlloyRecipes(consumer, Tin, 9, Antimony, 1, SolderingAlloy, 10, 500);
-        addAlloyRecipes(consumer, Lead, 4, Antimony, 1, BatteryAlloy, 5, 250);
-        addAlloyRecipes(consumer, Gold, 1, Silver, 1, Electrum, 2, 100);
-        addAlloyRecipes(consumer, Magnesium, 1, Aluminium, 2, Magnalium, 3, 150);
+        addAlloyRecipes(Copper, 3, Tin, 1, Bronze, 4, 200);
+        addAlloyRecipes(Copper, 3, Zinc, 1, Brass, 4, 200);
+        addAlloyRecipes(Copper, 1, Nickel, 1, Cupronickel, 2, 100);
+        addAlloyRecipes(Iron, 2, Nickel, 1, Invar, 3, 150);
+        addAlloyRecipes(Tin, 9, Antimony, 1, SolderingAlloy, 10, 500);
+        addAlloyRecipes(Lead, 4, Antimony, 1, BatteryAlloy, 5, 250);
+        addAlloyRecipes(Gold, 1, Silver, 1, Electrum, 2, 100);
+        addAlloyRecipes(Magnesium, 1, Aluminium, 2, Magnalium, 3, 150);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Redstone, 4), DUST.getMaterialIngredient(Copper, 1)).io(INGOT.get(RedAlloy, 1)).add("red_alloy_ingot", 50, 16);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(Redstone, 4), INGOT.getMaterialIngredient(Copper, 1)).io(INGOT.get(RedAlloy, 1)).add("red_alloy_ingot_1", 50, 16);
         //TODO compat for bluepower
@@ -47,7 +47,7 @@ public class AlloySmelterLoader {
 
     }
 
-    private static void addAlloyRecipes(Consumer<FinishedRecipe> consumer, Material input1, int count1, Material input2, int count2, Material output, int countO, int duration){
+    private static void addAlloyRecipes(Material input1, int count1, Material input2, int count2, Material output, int countO, int duration){
         ALLOY_SMELTING.RB().ii(INGOT.getMaterialIngredient(input1, count1), INGOT.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot", duration, 16);
         ALLOY_SMELTING.RB().ii(DUST.getMaterialIngredient(input1, count1), INGOT.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot_1", duration, 16);
         ALLOY_SMELTING.RB().ii(INGOT.getMaterialIngredient(input1, count1), DUST.getMaterialIngredient(input2, count2)).io(INGOT.get(output, countO)).add(output.getId() + "_ingot_2", duration, 16);
--- common/src/main/java/trinsdar/gt4r/loader/multi/CokePyrolysisOven.java
+++ common/src/main/java/trinsdar/gt4r/loader/multi/CokePyrolysisOven.java
@@ -15,9 +15,9 @@ import static trinsdar.gt4r.data.RecipeMaps.PYROLYSING;
 
 public class CokePyrolysisOven {
     public static void init(){
-        COKING.RB().ii(RecipeIngredient.of(Items.COAL, 1)).io(GEM.get(CoalCoke, 1)).fo(Creosote.getLiquid(500)).add(720);
-        COKING.RB().ii(RecipeIngredient.of(Items.COAL_BLOCK, 1)).io(BLOCK.get().get(CoalCoke).asStack()).fo(Creosote.getLiquid(4500)).add(1620);
-        COKING.RB().ii(RecipeIngredient.of(new ResourceLocation("minecraft", "logs"), 1)).io(new ItemStack(Items.CHARCOAL, 1)).fo(Creosote.getLiquid(250)).add(720);
-        PYROLYSING.RB().ii(RecipeIngredient.of(new ResourceLocation("minecraft", "logs"), 16)).io(new ItemStack(Items.CHARCOAL, 20)).fo(WoodGas.getGas(1500)).add(2400, 32);
+        COKING.RB().ii(RecipeIngredient.of(Items.COAL, 1)).io(GEM.get(CoalCoke, 1)).fo(Creosote.getLiquid(500)).add("coal_coke",720);
+        COKING.RB().ii(RecipeIngredient.of(Items.COAL_BLOCK, 1)).io(BLOCK.get().get(CoalCoke).asStack()).fo(Creosote.getLiquid(4500)).add("coal_coke_block",1620);
+        COKING.RB().ii(RecipeIngredient.of(new ResourceLocation("minecraft", "logs"), 1)).io(new ItemStack(Items.CHARCOAL, 1)).fo(Creosote.getLiquid(250)).add("charcoal",720);
+        PYROLYSING.RB().ii(RecipeIngredient.of(new ResourceLocation("minecraft", "logs"), 16)).io(new ItemStack(Items.CHARCOAL, 20)).fo(WoodGas.getGas(1500)).add("charcoal",2400, 32);
     }
 }
