4e23563771f5cd494fc9e161d8afe1c2cccb3351
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-30 00:54:19 -0400

    work on refactoring quantum chest storage method

--- common/src/main/java/trinsdar/gt4r/machine/QuantumChestItemHandler.java
+++ common/src/main/java/trinsdar/gt4r/machine/QuantumChestItemHandler.java
@@ -2,14 +2,23 @@ package trinsdar.gt4r.machine;
 
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import earth.terrarium.botarium.common.energy.util.EnergyHooks;
+import earth.terrarium.botarium.common.fluid.utils.FluidHooks;
 import muramasa.antimatter.capability.IGuiHandler;
 import muramasa.antimatter.capability.item.TrackedItemHandler;
 import muramasa.antimatter.capability.machine.MachineItemHandler;
+import muramasa.antimatter.gui.SlotType;
 import muramasa.antimatter.machine.event.ContentEvent;
+import muramasa.antimatter.util.AntimatterPlatformUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.NotNull;
+import tesseract.TesseractCapUtils;
 import trinsdar.gt4r.data.SlotTypes;
 import trinsdar.gt4r.blockentity.single.BlockEntityQuantumChest;
 
@@ -21,14 +30,15 @@ public class QuantumChestItemHandler extends MachineItemHandler<BlockEntityQuant
     int digitalCount;
     public QuantumChestItemHandler(BlockEntityQuantumChest tile) {
         super(tile);
-        int count = tile.getMachineType().getCount(tile.getMachineTier(), SlotTypes.QUANTUM);
-        inventories.put(SlotTypes.QUANTUM, new QuantumSlotTrackedHandler(tile, count, SlotTypes.QUANTUM.output, SlotTypes.QUANTUM.input, SlotTypes.QUANTUM.tester, SlotTypes.QUANTUM.ev, Integer.MAX_VALUE));
+        inventories.put(SlotType.STORAGE, new QuantumSlotTrackedHandler(tile, SlotType.STORAGE, 1, SlotType.STORAGE.output, SlotType.STORAGE.input, SlotType.STORAGE.tester, SlotType.STORAGE.ev, Integer.MAX_VALUE));
     }
 
     public static class QuantumSlotTrackedHandler extends TrackedItemHandler<BlockEntityQuantumChest> {
+        Item item;
+        int count;
 
-        public QuantumSlotTrackedHandler(BlockEntityQuantumChest tile, int size, boolean output, boolean input, BiPredicate<IGuiHandler, ItemStack> validator, ContentEvent contentEvent, int limit) {
-            super(tile, size, output, input, validator, contentEvent, limit);
+        public QuantumSlotTrackedHandler(BlockEntityQuantumChest tile, SlotType<?> type, int size, boolean output, boolean input, BiPredicate<IGuiHandler, ItemStack> validator, ContentEvent contentEvent, int limit) {
+            super(tile, type, size, output, input, validator, contentEvent, limit);
         }
 
         @Override
@@ -37,50 +47,73 @@ public class QuantumChestItemHandler extends MachineItemHandler<BlockEntityQuant
         }
 
         @Override
-        public CompoundTag serialize(CompoundTag nbt)
-        {
-            ListTag nbtTagList = new ListTag();
-            for (int i = 0; i < stacks.size(); i++)
-            {
-                if (!stacks.get(i).isEmpty())
-                {
-                    CompoundTag itemTag = new CompoundTag();
-                    itemTag.putInt("Slot", i);
-                    save(itemTag, stacks.get(i));
-                    nbtTagList.add(itemTag);
-                }
+        public CompoundTag serialize(CompoundTag nbt) {
+            if (item != Items.AIR) {
+                nbt.putString("Item", AntimatterPlatformUtils.getIdFromItem(item).toString());
             }
-            nbt.put("Items", nbtTagList);
+            nbt.putInt("count", count);
             return nbt;
         }
 
         @Override
-        public void deserialize(CompoundTag nbt)
-        {
-            ListTag tagList = nbt.getList("Items", Tag.TAG_COMPOUND);
-            for (int i = 0; i < tagList.size(); i++)
-            {
-                CompoundTag itemTags = tagList.getCompound(i);
-                int slot = itemTags.getInt("Slot");
-
-                if (slot >= 0 && slot < stacks.size())
-                {
-                    stacks.set(slot, of(itemTags));
-                }
+        public void deserialize(CompoundTag nbt) {
+            if (nbt.contains("Item")){
+                item = AntimatterPlatformUtils.getItemFromID(new ResourceLocation(nbt.getString("Item")));
             }
+            count = nbt.getInt("count");
             onLoad();
         }
 
+        @Override
+        public @NotNull ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
+            if (slot == 0){
+                if (stack.isEmpty() || stack.getTag() != null || FluidHooks.safeGetItemFluidManager(stack).isPresent() || TesseractCapUtils.getEnergyHandlerItem(stack).isPresent() || EnergyHooks.isEnergyItem(stack)) return stack;
+                if (item == Items.AIR){
+                    if (!simulate){
+                        item = stack.getItem();
+                        count = stack.getCount();
+                    }
+                    return ItemStack.EMPTY;
+                }
+                if (item != stack.getItem() || count == Integer.MAX_VALUE) return stack;
+                if ((long) count + (long) stack.getCount() <= Integer.MAX_VALUE){
+                    if (!simulate) {
+                        count += stack.getCount();
+                    }
+                    return ItemStack.EMPTY;
+                } else {
+                    int add = Integer.MAX_VALUE - count;
+                    if (!simulate){
+                        count += add;
+                    }
+                    return new ItemStack(stack.getItem(), stack.getCount() - add);
+                }
+            }
+            return super.insertItem(slot, stack, simulate);
+        }
 
-    }
-    @ExpectPlatform
-    public static CompoundTag save(CompoundTag pCompoundTag, ItemStack stack) {
-        throw new AssertionError();
-    }
+        @Override
+        public @NotNull ItemStack getItem(int slot) {
+            if (slot == 0){
+                return new ItemStack(item, count);
+            }
+            return super.getItem(slot);
+        }
 
-    @ExpectPlatform
-    public static ItemStack of(CompoundTag pCompoundTag) {
-        throw new AssertionError();
+        @Override
+        public @NotNull ItemStack extractItem(int slot, int amount, boolean simulate) {
+            if (slot == 0){
+                if (item == Items.AIR || count == 0){
+                    return ItemStack.EMPTY;
+                }
+                int toRemove = Math.min(amount, count);
+                if (!simulate){
+                    count -= toRemove;
+                }
+                return new ItemStack(item, toRemove);
+            }
+            return super.extractItem(slot, amount, simulate);
+        }
     }
 
     /*public void drawInfo(MatrixStack stack, FontRenderer renderer, int left, int top) {
