f61593d4476ae055f94bc903807b038c2d137fe6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-11-26 17:00:54 -0500

    switched to infinite slot handler fromg t core for quantum chest

--- common/src/main/java/trinsdar/gt4r/data/MenuHandlers.java
+++ common/src/main/java/trinsdar/gt4r/data/MenuHandlers.java
@@ -16,21 +16,6 @@ import trinsdar.gt4r.blockentity.single.BlockEntityDigitalChest;
 import trinsdar.gt4r.blockentity.single.BlockEntityQuantumChest;
 
 public class MenuHandlers {
-    public static MenuHandlerMachine<BlockEntityFusionReactor, ? extends ContainerMultiMachine> FUSION_MENU_HANDLER = new MenuHandlerMachine(GT4RRef.ID, "container_fusion_reactor") {
-        @Override
-        public ContainerMultiMachine getMenu(IGuiHandler tile, Inventory playerInv, int windowId) {
-            return tile instanceof BlockEntityMachine ? new ContainerMultiMachine((BlockEntityMultiMachine<?>) tile, playerInv, this, windowId) : null;
-        }
-        @Override
-        public String screenDomain() {
-            return GT4RRef.ID;
-        }
-
-        @Override
-        public String screenID() {
-            return "fusion_reactor";
-        }
-    };
 
     public static MenuHandlerMachine<BlockEntityQuantumChest, ContainerQuantumChest> QUANTUM_CHEST_HANDLER = new MenuHandlerMachine(GT4RRef.ID, "container_quantum_chest") {
         @Override
--- common/src/main/java/trinsdar/gt4r/machine/QuantumChestItemHandler.java
+++ common/src/main/java/trinsdar/gt4r/machine/QuantumChestItemHandler.java
@@ -1,76 +1,17 @@
 package trinsdar.gt4r.machine;
 
 
-import dev.architectury.injectables.annotations.ExpectPlatform;
-import muramasa.antimatter.capability.IGuiHandler;
-import muramasa.antimatter.capability.item.TrackedItemHandler;
+import io.github.gregtechintergalactical.gtcore.data.SlotTypes;
+import io.github.gregtechintergalactical.gtcore.machine.InfiniteSlotTrackedHandler;
 import muramasa.antimatter.capability.machine.MachineItemHandler;
-import muramasa.antimatter.gui.SlotType;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
-import net.minecraft.world.item.ItemStack;
-import trinsdar.gt4r.data.SlotTypes;
 import trinsdar.gt4r.blockentity.single.BlockEntityQuantumChest;
-
-import javax.annotation.Nonnull;
-import java.util.function.BiPredicate;
 
 public class QuantumChestItemHandler extends MachineItemHandler<BlockEntityQuantumChest> {
     int maxSize = Integer.MAX_VALUE;
     int digitalCount;
     public QuantumChestItemHandler(BlockEntityQuantumChest tile) {
         super(tile);
-        int count = tile.getMachineType().getCount(tile.getMachineTier(), SlotTypes.QUANTUM);
-        inventories.put(SlotTypes.QUANTUM, new QuantumSlotTrackedHandler(tile, SlotTypes.QUANTUM, count, SlotTypes.QUANTUM.output, SlotTypes.QUANTUM.input, SlotTypes.QUANTUM.tester, Integer.MAX_VALUE));
+        int count = tile.getMachineType().getCount(tile.getMachineTier(), SlotTypes.UNLIMITED);
+        inventories.put(SlotTypes.UNLIMITED, new InfiniteSlotTrackedHandler<>(tile, SlotTypes.UNLIMITED, count, SlotTypes.UNLIMITED.output, SlotTypes.UNLIMITED.input, SlotTypes.UNLIMITED.tester, Integer.MAX_VALUE));
     }
-
-    public static class QuantumSlotTrackedHandler extends TrackedItemHandler<BlockEntityQuantumChest> {
-
-        public QuantumSlotTrackedHandler(BlockEntityQuantumChest tile, SlotType<?> type, int size, boolean output, boolean input, BiPredicate<IGuiHandler, ItemStack> validator, int limit) {
-            super(tile, type, size, output, input, validator, limit);
-        }
-
-        @Override
-        protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
-            return stack.getMaxStackSize() == 1 ? stack.getMaxStackSize() : getSlotLimit(slot);
-        }
-
-        @Override
-        public CompoundTag serialize(CompoundTag nbt) {
-            ListTag nbtTagList = new ListTag();
-            for (int i = 0; i < stacks.size(); i++) {
-                if (!stacks.get(i).isEmpty()) {
-                    CompoundTag itemTag = new CompoundTag();
-                    itemTag.putInt("Slot", i);
-                    stacks.get(i).save(itemTag);
-                    itemTag.putInt("count", stacks.get(i).getCount());
-                    nbtTagList.add(itemTag);
-                }
-            }
-            nbt.put("Items", nbtTagList);
-            return nbt;
-        }
-
-        @Override
-        public void deserialize(CompoundTag nbt) {
-            ListTag tagList = nbt.getList("Items", Tag.TAG_COMPOUND);
-            for (int i = 0; i < tagList.size(); i++) {
-                CompoundTag itemTags = tagList.getCompound(i);
-                int slot = itemTags.getInt("Slot");
-                if (slot >= 0 && slot < stacks.size()) {
-                    stacks.set(slot, ItemStack.of(itemTags));
-                    stacks.get(slot).setCount(itemTags.getInt("count"));
-                }
-            }
-            onLoad();
-        }
-
-
-    }
-
-    /*public void drawInfo(MatrixStack stack, FontRenderer renderer, int left, int top) {
-        // TODO: Replace by new TranslationTextComponent()
-        renderer.draw(stack,"Item amount: " + digitalCount, left + 10, top + 19, 16448255);
-    }*/
 }
