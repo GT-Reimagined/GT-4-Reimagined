393a143d5afe4156bfb36cfa76c8ed24ac5c56b4
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-01-23 23:55:19 -0500

    updated antimatter, removed duplicate recipes, disabled fusion recipes for now also removed plasmas

--- common/src/main/java/trinsdar/gt4r/loader/crafting/WoodCrafting.java
+++ common/src/main/java/trinsdar/gt4r/loader/crafting/WoodCrafting.java
@@ -37,19 +37,6 @@ public class WoodCrafting {
             }
 
         }
-        String[] stones = {"stone", "smooth_stone", "sandstone", "cut_sandstone", "cobblestone", "red_sandstone", "cut_red_sandstone", "prismarine", "dark_prismarine", "polished_granite", "smooth_red_sandstone", "polished_diorite", "mossy_cobblestone", "smooth_sandstone", "smooth_quartz", "granite", "andesite", "polished_andesite", "diorite", "blackstone", "polished_blackstone", "purpur", "quartz", "brick", "stone_brick", "nether_brick", "prismarine_brick", "mossy_stone_brick", "end_stone_brick", "red_nether_brick", "polished_blackstone_brick"};
-        for (String stone : stones) {
-            Item full = AntimatterPlatformUtils.getItemFromID(new ResourceLocation(stone + (stone.equals("purpur") || stone.equals("quartz") ? "_block" : stone.contains("brick") ? "s" : "")));
-            Item slab = AntimatterPlatformUtils.getItemFromID(new ResourceLocation(stone + "_slab"));
-            String[] pattern = stone.equals("purpur") || stone.equals("quartz") || stone.equals("sandstone") || stone.equals("red_sandstone") || stone.equals("stone_brick") || stone.equals("nether_brick") || stone.equals("polished_blackstone") ? new String[]{"SS"} : new String[]{"S", "S"};
-            provider.addItemRecipe(consumer, GT4RRef.ID, stone + "_slab_to_" + stone, "slabs", full, of('S', slab), pattern);
-        }
-        String[] wood = {"oak", "birch", "spruce", "jungle", "acacia", "dark_oak", "crimson", "warped"};
-        for (String s : wood) {
-            ResourceLocation name = new ResourceLocation(s + "_planks");
-            ResourceLocation slab = new ResourceLocation(s + "_slab");
-            provider.addItemRecipe(consumer, GT4RRef.ID, slab.getPath() + "_to_" + name.getPath(), "slabs", AntimatterPlatformUtils.getItemFromID(name), of('S', AntimatterPlatformUtils.getItemFromID(slab)), "S", "S");
-        }
         if (AntimatterAPI.isModLoaded(GT4RRef.MOD_TERRESTRIA)){
             String[] woodTypes = {"cypress", "hemlock", "japanese_maple", "rainbow_eucalyptus", "redwood", "rubber", "sakura", "willow", "yucca_palm"};
             for (String woodType : woodTypes) {
