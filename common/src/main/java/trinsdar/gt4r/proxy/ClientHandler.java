c9de9550eb51969b80da231e252e857fff1484cc
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-04-28 23:03:20 -0400

    did a little work on included resource packs

--- common/src/main/java/trinsdar/gt4r/proxy/ClientHandler.java
+++ common/src/main/java/trinsdar/gt4r/proxy/ClientHandler.java
@@ -3,11 +3,16 @@ package trinsdar.gt4r.proxy;
 import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.client.ModelUtils;
 import muramasa.antimatter.proxy.IProxyHandler;
+import muramasa.antimatter.util.AntimatterPlatformUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.server.packs.AbstractPackResources;
+import net.minecraft.server.packs.FilePackResources;
+import net.minecraft.server.packs.PackResources;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.Level;
+import org.apache.commons.io.FileUtils;
 import trinsdar.gt4r.GT4Reimagined;
 import trinsdar.gt4r.block.BlockCasing;
 import trinsdar.gt4r.block.BlockRedstoneMachine;
@@ -36,19 +41,28 @@ public class ClientHandler implements IProxyHandler {
     }
 
     private static void copyProgrammerArtIfMissing() {
-        File dir = new File(".", "resourcepacks");
+        File dir = new File(AntimatterPlatformUtils.getConfigDir().getParent().toFile(), "resourcepacks");
         File target = new File(dir, "GT4R-Programmer-Art.zip");
         File target2 = new File(dir, "GT4R-Classic-Tools.zip");
+        File target3 = new File(dir, "GT4R-Machine-Alt.zip");
 
 
         //if(!target.exists())
             try {
                 dir.mkdirs();
-                InputStream in = GT4Reimagined.class.getResourceAsStream("/assets/gt4r/programmer_art.zip");
-                FileOutputStream out = new FileOutputStream(target);
-
+                InputStream in = GT4Reimagined.class.getResourceAsStream("/assets/gt4r/gt4r-machine-alt.zip");
+                FileOutputStream out = new FileOutputStream(target3);
                 byte[] buf = new byte[16384];
                 int len = 0;
+                while((len = in.read(buf)) > 0)
+                    out.write(buf, 0, len);
+
+                in.close();
+                out.close();
+
+                in = GT4Reimagined.class.getResourceAsStream("/assets/gt4r/programmer_art.zip");
+                out = new FileOutputStream(target);
+                buf = new byte[16384];
                 while((len = in.read(buf)) > 0)
                     out.write(buf, 0, len);
 
@@ -57,7 +71,6 @@ public class ClientHandler implements IProxyHandler {
                 in = GT4Reimagined.class.getResourceAsStream("/assets/gt4r/classic_tools.zip");
                 out = new FileOutputStream(target2);
                 buf = new byte[16384];
-                len = 0;
                 while((len = in.read(buf)) > 0)
                     out.write(buf, 0, len);
 
