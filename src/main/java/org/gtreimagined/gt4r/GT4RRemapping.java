6b6ea6105968aff7ab65eb25a309e444e7f91e37
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-08 21:28:51 -0500

    remapped gt core batteries back to gt4r batteries

--- src/main/java/org/gtreimagined/gt4r/GT4RRemapping.java
+++ src/main/java/org/gtreimagined/gt4r/GT4RRemapping.java
@@ -1,6 +1,8 @@
 package org.gtreimagined.gt4r;
 
 import muramasa.antimatter.AntimatterRemapping;
+import net.minecraft.resources.ResourceLocation;
+import org.gtreimagined.gtcore.GTCore;
 
 import static org.gtreimagined.gt4r.data.Machines.FLUID_PRESS;
 import static org.gtreimagined.gt4r.data.Machines.SOLID_FUEL_BOILER;
@@ -9,5 +11,11 @@ public class GT4RRemapping {
     public static void init(){
         AntimatterRemapping.remapMachine("coal_boiler", SOLID_FUEL_BOILER);
         AntimatterRemapping.remapMachine("fluid_extractor", FLUID_PRESS);
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "small_battery_hull"), new ResourceLocation(GT4RRef.ID, "battery_hull"));
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "re_battery"), new ResourceLocation(GT4RRef.ID, "re_battery"));
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "energy_crystal"), new ResourceLocation(GT4RRef.ID, "energy_crystal"));
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "small_lithium_battery"), new ResourceLocation(GT4RRef.ID, "lithium_battery"));
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "lapotron_crystal"), new ResourceLocation(GT4RRef.ID, "lapotron_crystal"));
+        AntimatterRemapping.remap(new ResourceLocation(GTCore.ID, "lapotronic_energy_orb"), new ResourceLocation(GT4RRef.ID, "lapotronic_energy_orb"));
     }
 }
