03d630dd5877238091cd69144fdc223d80c1376e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-11 19:08:37 -0400

    made reactor chambers unable to remain placed without a reactor core

--- src/main/java/org/gtreimagined/gt4r/block/BlockReactorChamber.java
+++ src/main/java/org/gtreimagined/gt4r/block/BlockReactorChamber.java
@@ -6,7 +6,9 @@ import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.machine.types.Machine;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.gtreimagined.gt4r.data.Machines;
 
@@ -25,5 +27,10 @@ public class BlockReactorChamber extends BlockMachine {
             }
         }
         return reactors == 1;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
+        return !state.canSurvive(level, currentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
     }
 }
