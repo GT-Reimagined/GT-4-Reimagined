190bbe5a8afd4f9af49373709abdb957618da388
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-11 19:55:53 -0400

    made reactor chambers set their reactors on first tick

--- src/main/java/org/gtreimagined/gt4r/reactor/tile/BlockEntityReactorChamber.java
+++ src/main/java/org/gtreimagined/gt4r/reactor/tile/BlockEntityReactorChamber.java
@@ -1,29 +1,66 @@
 package org.gtreimagined.gt4r.reactor.tile;
 
+import lombok.Getter;
 import muramasa.antimatter.blockentity.BlockEntityFakeBlock;
 import muramasa.antimatter.blockentity.BlockEntityMachine;
+import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.tool.AntimatterToolType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.network.NetworkHooks;
+import org.gtreimagined.gt4r.data.Machines;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockEntityReactorChamber extends BlockEntityMachine<BlockEntityReactorChamber> {
-    BlockEntityReactorCore reactor;
+    @Getter
+    private BlockEntityReactorCore reactor;
+    private Direction reactorSide;
 
     public BlockEntityReactorChamber(Machine<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+    }
+
+    @Override
+    public void onFirstTickServer(Level level, BlockPos pos, BlockState state) {
+        super.onFirstTickServer(level, pos, state);
+        if (reactor == null) {
+            for (var d : Direction.values()) {
+                BlockEntity be = level.getBlockEntity(pos.relative(d));
+                if (be instanceof BlockEntityReactorCore core){
+                    setReactor(core);
+                    reactorSide = d;
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onFirstTickClient(Level level, BlockPos pos, BlockState state) {
+        super.onFirstTickClient(level, pos, state);
+        if (reactor == null) {
+            for (var d : Direction.values()) {
+                BlockEntity be = level.getBlockEntity(pos.relative(d));
+                if (be instanceof BlockEntityReactorCore core){
+                    reactor = core;
+                    reactorSide = d;
+                    break;
+                }
+            }
+        }
     }
 
     @Override
@@ -49,5 +86,8 @@ public class BlockEntityReactorChamber extends BlockEntityMachine<BlockEntityRea
 
     public void setReactor(BlockEntityReactorCore reactor) {
         this.reactor = reactor;
+        if (this.level != null && isServerSide()) {
+            sidedSync(true);
+        }
     }
 }
--- src/main/java/org/gtreimagined/gt4r/reactor/tile/BlockEntityReactorCore.java
+++ src/main/java/org/gtreimagined/gt4r/reactor/tile/BlockEntityReactorCore.java
@@ -31,6 +31,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion.BlockInteraction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.fluids.FluidStack;
@@ -235,7 +236,7 @@ public class BlockEntityReactorCore extends BlockEntityBasicMultiMachine<BlockEn
         updateTag.putBoolean("isFluid", isFluid);
     }
 
-    public static int getAttachedChambers(Level worldIn, BlockPos pos) {
+    public int getAttachedChambers(Level worldIn, BlockPos pos) {
         int chamberCount = 0;
 
         for (var d : Direction.values()) {
