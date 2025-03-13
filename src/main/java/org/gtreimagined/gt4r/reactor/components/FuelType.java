e1f8d82888908c105ac7e3eeeb1fbc6740e7a447
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-13 18:21:10 -0400

    reorded params in FuelType and added a couple more params

--- src/main/java/org/gtreimagined/gt4r/reactor/components/FuelType.java
+++ src/main/java/org/gtreimagined/gt4r/reactor/components/FuelType.java
@@ -1,12 +1,18 @@
 package org.gtreimagined.gt4r.reactor.components;
 
+import com.google.common.collect.ImmutableList;
+import muramasa.antimatter.util.int2;
 import org.gtreimagined.gt4r.reactor.Config;
 
-public record FuelType(double energyMult, double heatMult, int pulsesPerTick, boolean isMox, int durability, double explosionMult, double moxHeatCoefficient, double moxEUCoefficient) {
+import java.util.List;
 
-    public static final FuelType URANIUM = new FuelType(1.0, 4.0, 1, false, 10000, 1.0);
+public record FuelType(int durability, double energyMult, double heatMult, int pulsesPerTick,  int connectivityPulses, boolean isMox, double explosionMult, double moxHeatCoefficient, double moxEUCoefficient, List<int2> pulseArea, List<int2> heatPulseArea) {
+    public static final List<int2> DEFAULT_AREA = ImmutableList.of(new int2(-1, 0), new int2(1, 0), new int2(0, -1), new int2(0, 1));
+    public static final List<int2> LARGE_PULSES_AREA = ImmutableList.of(new int2(-1, 0), new int2(1, 0), new int2(0, -1), new int2(0, 1), new int2(-1, -1), new int2(1, -1), new int2(-1, 1), new int2(1, 1));
 
-    public FuelType(double energyMult, double heatMult, int pulsesPerTick, boolean isMox, int durability, double explosionMult){
-        this(energyMult, heatMult, pulsesPerTick, isMox, durability, explosionMult, 2.0, Config.MOX_EU_COEFFICIENT);
+    public static final FuelType URANIUM = new FuelType(10000, 1.0, 4.0, 1, 1, false, 1.0);
+
+    public FuelType(int durability, double energyMult, double heatMult, int pulsesPerTick, int connectivityPulses, boolean isMox, double explosionMult){
+        this(durability, energyMult, heatMult, pulsesPerTick, connectivityPulses, isMox, explosionMult, 2.0, Config.MOX_EU_COEFFICIENT, DEFAULT_AREA, DEFAULT_AREA);
     }
 }
