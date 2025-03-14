f5151f803fda4491505278e95eba9721e5f92e24
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-14 17:10:15 -0400

    made lava placing only place flowing lava, not lava sources

--- src/main/java/org/gtreimagined/gt4r/blockentity/multi/BlockEntityReactorCore.java
+++ src/main/java/org/gtreimagined/gt4r/blockentity/multi/BlockEntityReactorCore.java
@@ -418,7 +418,7 @@ public class BlockEntityReactorCore extends BlockEntityBasicMultiMachine<BlockEn
                 BlockPos pos = new BlockPos(x, y, z);
                 var block = level.getBlockState(pos);
                 if (!block.isAir() && block.getDestroySpeed(level, pos) < 5){
-                    level.setBlock(pos, Blocks.LAVA.defaultBlockState(), 3);
+                    level.setBlock(pos, Blocks.LAVA.defaultBlockState().setValue(LiquidBlock.LEVEL, 7), 3);
                     level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1.0F, 1.0F);
                     break;
                 }
